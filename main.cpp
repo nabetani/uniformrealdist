55e52facefa358810e078507fba9a606c16032f9
Nabetani <takenori@nabetani.sakura.ne.jp>
2021-09-26 10:55:08 +0900

    シンプルにする、他の cpp に対応

--- main.cpp
+++ main.cpp
@@ -1,27 +1,23 @@
-#include <iostream>
+#include <cstdio>
 #include <random>
 #include <cmath>
 
 int main(int argc, char *argv[])
 {
-  using real = float;
-  std::random_device seed_gen;
-  std::mt19937_64 engine(seed_gen());
-  std::uniform_real_distribution<real> dist(1.0f-1.0f/(1u<<16), 1.0f);
+  std::mt19937_64 engine(0);
+  auto delta = 1.0/(1ull<<50);
+  auto maxval = 1+delta;
+  auto minval = 1-delta;
+  std::uniform_real_distribution<> dist(minval,maxval);
 
-  int count=0;
-  unsigned long long TRIAL = (1ull<<20);
-  for (unsigned long long i = 0; i < TRIAL; i++)
+  constexpr unsigned long long TRIAL = 1ull<<24;
+  for (unsigned long long i = 0; i<TRIAL; i++)
   {
     auto v = dist(engine);
-    if (v==1){
-      ++count;
+    if (v<minval || maxval<=v ){
+      std::printf("v=%.17f  range is [%.17f, %.17f)", v, minval, maxval );
+      return 0;
     }
-  }
-  if (count==0 ){
-      std::cout << "no max value" << std::endl;
-  } else {
-      std::cout << (std::log2(TRIAL)-std::log2(count)) << std::endl;
   }
   return 0;
 }
--- run.rb
+++ run.rb
@@ -1,16 +1,17 @@
 require "fileutils"
 
-def test(cc,name)
+def test(cpp, cc, name)
   dir = "bin/"+name
   FileUtils.mkdir_p(dir)
   Dir.chdir(dir) do
-    puts("#{name}: " + %x(#{cc} -Wall -O2 -std=c++11 ../../main.cpp && ./a.out))
-    p $?
+    puts("#{name}: " + %x(#{cc} -Wall -O2 -std=c++11 ../../#{cpp} && ./a.out))
   end
 end
 
+cpp = ARGV[0] || "main.cpp"
+puts cpp
 Dir.chdir( File.split(__FILE__).first ) do
-  test( "clang++", "clang" )
-  test( "g++-11", "gcc11" )
-  test( "g++-7", "gcc7" )
+  test( cpp, "clang++", "clang" )
+  test( cpp, "g++-11", "gcc11" )
+  test( cpp, "g++-7", "gcc7" )
 end
